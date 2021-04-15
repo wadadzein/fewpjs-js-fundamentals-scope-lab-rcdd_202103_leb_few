f4db8e33a6bbaec912ccf96f0cceb49be5b9257e
wadad zein <wadad_zein@hotmail.com>
2021-04-15 09:14:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-animal = ""
+const animal = "cat"
 
 function myAnimal() {
-  return animal
+  return animal;
 }
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return animal;
 }
 
 function add2(n) {
-  return n + two
+  let two = 2;
+  return n + two;
 
   // Feel free to move things around!
   two = 2
